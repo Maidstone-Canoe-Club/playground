c37387cb3385b3c5953ec0bffddba535f066b5c3
Matt <34515355+matt-clegg@users.noreply.github.com>
2023-04-12 07:26:02 +0100

    Install script (#4)
    * fix: removed docker login from install script
    
    * fix: ensure directory exists
    
    * fix: removed sudo

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
-sudo mkdir ~/docker
+mkdir ~/docker
 
 # Set the working directory to the directory containing the docker-compose.yml file
 cd ~/docker || echo "Could not set working directory to docker" exit
 
-sudo wget "https://raw.githubusercontent.com/Maidstone-Canoe-Club/playground/main/docker-compose.yml" -O "/docker-compose.yml"
+wget "https://raw.githubusercontent.com/Maidstone-Canoe-Club/playground/main/docker-compose.yml" -O "/docker-compose.yml"
 echo "Docker compose file updated"
 
 # Pull the latest versions of the images
-sudo docker compose pull
+docker compose pull
 
 # Recreate the containers with the new images
-sudo docker compose up -d
+docker compose up -d
