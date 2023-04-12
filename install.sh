3889ffb2f4a6f3ecc58d962758c6a3157b140a5d
Matt <mattclegg93@gmail.com>
2023-04-12 07:21:10 +0100

    fix: ensure directory exists

--- install.sh
+++ install.sh
@@ -1,16 +1,13 @@
-﻿#!/bin/bash
+﻿sudo mkdir ~/docker
 
 # Set the working directory to the directory containing the docker-compose.yml file
-cd ~/docker || echo "Could not set working directory to docker"exit
+cd ~/docker || echo "Could not set working directory to docker" exit
 
-FILE_URL="https://raw.githubusercontent.com/Maidstone-Canoe-Club/playground/main/docker-compose.yml"
-FILE_PATH="/docker-compose.yml"
-
-wget "${FILE_URL}" -O "${FILE_PATH}"
+sudo wget "https://raw.githubusercontent.com/Maidstone-Canoe-Club/playground/main/docker-compose.yml" -O "/docker-compose.yml"
 echo "Docker compose file updated"
 
 # Pull the latest versions of the images
-docker compose pull
+sudo docker compose pull
 
 # Recreate the containers with the new images
-docker compose up -d
+sudo docker compose up -d
