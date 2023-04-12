8d298aaccb25ed8773e2137333495bab6ccf4d7c
Matt <mattclegg93@gmail.com>
2023-04-12 07:32:11 +0100

    fix: sudo fix

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ mkdir ~/docker
 # Set the working directory to the directory containing the docker-compose.yml file
 cd ~/docker || echo "Could not set working directory to docker" exit
 
-wget "https://raw.githubusercontent.com/Maidstone-Canoe-Club/playground/main/docker-compose.yml" -O "/docker-compose.yml"
+sudo su -c 'wget "https://raw.githubusercontent.com/Maidstone-Canoe-Club/playground/main/docker-compose.yml" -O "/docker-compose.yml"'
 echo "Docker compose file updated"
 
 # Pull the latest versions of the images
-docker compose pull
+sudo su -c 'docker compose pull'
 
 # Recreate the containers with the new images
-docker compose up -d
+suddo su -c 'docker compose up -d'
