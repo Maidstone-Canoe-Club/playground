304b40d27057b600263febf1f09aa0365727e606
Matt <34515355+matt-clegg@users.noreply.github.com>
2023-04-13 16:38:49 +0100

    Docker fixes (#10)
    * fix: fixed prod docker issues
    
    * fix: more live fixes
    
    * fix: more fixes
    
    * fix: url check
    
    * fix: more docker fixes
    
    * fix: stop containers

--- install.sh
+++ install.sh
@@ -3,11 +3,14 @@ mkdir -p ~/docker
 # Set the working directory to the directory containing the docker-compose.yml file
 cd ~/docker || echo "Could not set working directory to docker" exit
 
-wget -O docker-compose.yml https://raw.githubusercontent.com/Maidstone-Canoe-Club/playground/main/docker-compose.yml
 wget -O docker-compose.prod.yml https://raw.githubusercontent.com/Maidstone-Canoe-Club/playground/main/docker-compose.prod.yml
+
+docker compose stop
+
+docker compose rm -f
 
 # Pull the latest versions of the images
 docker compose pull
 
 # Recreate the containers with the new images
-docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --remove-orphans --force-recreate
+docker compose -f docker-compose.prod.yml up -d --remove-orphans --force-recreate
