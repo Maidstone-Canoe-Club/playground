dbbf93bf1e590c9fb28c393a505bf124101c9511
Matt <34515355+matt-clegg@users.noreply.github.com>
2023-04-13 16:24:49 +0100

    Docker fixes (#9)
    * fix: fixed prod docker issues
    
    * fix: more live fixes
    
    * fix: more fixes
    
    * fix: url check

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -5,15 +5,4 @@
 <script setup>
 const url = useDirectusUrl();
 console.log("DIRECTUS_URL", url);
-const { getSingletonItem } = useDirectusItems();
-
-try {
-  const { data } = await useAsyncData("global", () => {
-    return getSingletonItem({
-      collection: "global"
-    });
-  });
-} catch (err) {
-  console.error("error", err);
-}
 </script>
