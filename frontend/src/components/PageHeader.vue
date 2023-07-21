acde6b9033ceac6eccf572034f85171a9389edec
Matt <mattclegg93@gmail.com>
2023-07-21 10:47:19 +0100

    fix: fixed sign out button on page header

--- frontend/src/components/PageHeader.vue
+++ frontend/src/components/PageHeader.vue
@@ -96,14 +96,24 @@
           </div>
         </div>
         <div class="mt-3 space-y-1">
-          <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href" class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800">
+          <DisclosureButton
+            v-for="item in userNavigation"
+            :key="item.name"
+            as="a"
+            :href="item.href"
+            class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
+            @click="item.onClick">
             {{ item.name }}
           </DisclosureButton>
         </div>
       </div>
       <div v-else>
         <div class="mt-3 space-y-1">
-          <DisclosureButton :as="NuxtLink" to="/login" class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800">
+          <DisclosureButton
+            as="a"
+            href="javascript:;"
+            class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
+            @click="navigateTo('/login')">
             Login
           </DisclosureButton>
         </div>
