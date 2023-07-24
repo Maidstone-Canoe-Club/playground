44f344df55e7ff1cfe8f88d40f75a0bcd9857a85
Matt <mattclegg93@gmail.com>
2023-07-24 21:09:02 +0100

    fix: added color weights

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -5,7 +5,20 @@ export default <Partial<Config>>{
   theme: {
     extend: {
       colors: {
-        brand: "#2D327D"
+        brand: {
+          50: "#eef4ff",
+          100: "#e0eaff",
+          200: "#c7d7fe",
+          300: "#a5bdfc",
+          400: "#8198f8",
+          500: "#6475f0",
+          600: "#474de4",
+          700: "#393dc9",
+          800: "#3034a3",
+          900: "#2d327d",
+          950: "#1b1d4b"
+        }
+
       }
     }
   },
